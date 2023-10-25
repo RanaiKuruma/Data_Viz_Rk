06d38706e92100eb20e87d9e2f5dd4849b1d8d7c
Ranai Kuruma <105557429+RanaiKuruma@users.noreply.github.com>
2023-10-25 10:12:45 +0530

    Update mean_and_median.py

--- Statistics_Python/mean_and_median.py
+++ Statistics_Python/mean_and_median.py
@@ -14,3 +14,28 @@ print(np.median(be_consumption['consumption']))
 # Calculate mean and median consumption in USA
 print(np.mean(usa_consumption['consumption']))
 print(np.median(usa_consumption['consumption']))
+
+# Import numpy as np
+import numpy as np
+
+# Subset for Belgium and USA only
+be_and_usa = food_consumption[(food_consumption['country'] == 'Belgium')| (food_consumption['country'] == 'USA')]
+
+# Group by country, select consumption column, and compute mean and median
+print(be_and_usa.groupby('country')['consumption'].agg([np.mean, np.median]))
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt 
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category'] == 'rice']
+
+# Histogram of co2_emission for rice and show plot
+plt.hist(rice_consumption['co2_emission'])
+plt.show()
+
+# Subset for food_category equals rice
+rice_consumption = food_consumption[food_consumption['food_category'] == 'rice']
+
+# Calculate mean and median of co2_emission with .agg()
+print(rice_consumption['co2_emission'].agg([np.mean, np.median]))
